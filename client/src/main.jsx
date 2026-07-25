4beea26d47994521ba0f005df16401e1f99ae934
Chandmanish-web <manish2003ban@gmail.com>
2026-07-25 10:40:05 +0530

    Add GitHub Pages deployment config for frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/store';
@@ -22,9 +22,9 @@ if (savedAuth) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
